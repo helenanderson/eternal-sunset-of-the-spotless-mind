deb441892db70e585d787c7157b3f7ab4dd2ac4a
helenanderson <helenanderson@stanford.edu>
2016-07-29 16:18:22 -0700

    simple twitter embed

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 from flask import Flask
 from threading import Timer
-from fetch_scope import most_recent_live_scope
+from fetch_scope import most_recent_live_scope, scope_is_live
+import requests
 
 app = Flask(__name__)
+
+# There's probably a better way to do this where we don't have to keep track of both of these things globally, but for now...
+current_tweet = None
 current_scope = None
 INTERVAL = 15.0
 
 @app.route("/")
 def home():
-	global current_scope
-	return current_scope
+	return constructEmbedHTML()
+
+def constructEmbedHTML():
+	global current_tweet
+	embed_url = 'https://api.twitter.com/1.1/statuses/oembed.json?id=' + str(current_tweet.id)
+	oembed = requests.get(embed_url).json()
+	return oembed['html']
 
 def updateScope():	
 	global current_scope 
-	current_scope = most_recent_live_scope()
+	global current_tweet
+
+	# If the current scope is still live, don't bother changing it
+	if current_scope is None or not scope_is_live(current_scope):
+		current_tweet, current_scope = most_recent_live_scope()
 
 	timer = Timer(INTERVAL, updateScope)
 	timer.start()
--- fetch_scope.py
+++ fetch_scope.py
@@ -2,6 +2,7 @@ from os import path
 
 import requests
 import tweepy
+import datetime
 
 QUERY = 'periscope sunset'
 
@@ -18,22 +19,25 @@ def load_auth():
 twitter_auth = load_auth()
 twitter = tweepy.API(twitter_auth)
 
+# Returns tweet object
 def most_recent_live_scope():
-    tweets = twitter.search(q=QUERY, count=10)
+    tweets = twitter.search(q=QUERY)
     for tweet in tweets:
         print tweet.text
         for url in tweet.entities['urls']:
             expanded_url = url['expanded_url']
             if expanded_url.startswith('https://www.periscope.tv/w/'):
-                scope_source = requests.get(expanded_url)
-                if scope_is_live(scope_source):
-                    return expanded_url
+                if scope_is_live(expanded_url):
+                    return tweet, expanded_url
     for tweet in tweets:
         for url in tweet.entities['urls']:
             expanded_url = url['expanded_url']
             if expanded_url.startswith('https://www.periscope.tv/w/'):
-                return expanded_url
+                return tweet, expanded_url
     return "no scope found :("
 
-def scope_is_live(scope_source):
-    return 'isEnded&quot;:false' in scope_source
+def scope_is_live(expanded_url):
+    return 'isEnded&quot;:false' in requests.get(expanded_url)
+
+def scope_in_timeframe(tweet, hours):
+    return datetime.datetime.now() - tweet.created_at < datetime.timedelta(hours = hours)
