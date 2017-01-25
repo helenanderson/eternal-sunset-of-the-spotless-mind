44cdeb1b8d9fabf2e0dfd282ba32e9d94a12aba7
helenanderson <helenanderson@stanford.edu>
2017-01-24 23:21:38 -0800

    first deployment

--- app.py
+++ app.py
@@ -1,36 +1,43 @@
 from flask import Flask
 from threading import Timer
-from fetch_scope import most_recent_live_scope, scope_is_live
+from fetch_scope import most_recent_scope, scope_is_live
+from flask import render_template, jsonify
 import requests
 
 app = Flask(__name__)
 
 # There's probably a better way to do this where we don't have to keep track of both of these things globally, but for now...
-current_tweet = None
-current_scope = None
-INTERVAL = 15.0
+current_scopes = {
+	'sunrise': None,
+	'sunset': None
+}
+INTERVAL = 60.0
+
+@app.route('/_sets')
+def sets():
+	return jsonify(result=current_scopes['sunset'])
+
+@app.route('/_rises')
+def rises():
+	return jsonify(result=current_scopes['sunrise'])
 
 @app.route("/")
 def home():
-	return constructEmbedHTML()
+	global current_scopes
+	return render_template('home.html', scope=current_scopes['sunrise'])
 
-def constructEmbedHTML():
-	global current_tweet
-	embed_url = 'https://api.twitter.com/1.1/statuses/oembed.json?id=' + str(current_tweet.id)
-	oembed = requests.get(embed_url).json()
-	return oembed['html']
+def updateScopes():
+	global current_scopes
 
-def updateScope():	
-	global current_scope 
-	global current_tweet
+	for key in current_scopes:
+		# If the current scope is still live, don't bother changing it
+		current_scope = current_scopes[key]
+		if current_scope is None or not scope_is_live(requests.get(current_scope)):
+			current_scopes[key] = most_recent_scope(key)
 
-	# If the current scope is still live, don't bother changing it
-	if current_scope is None or not scope_is_live(current_scope):
-		current_tweet, current_scope = most_recent_live_scope()
-
-	timer = Timer(INTERVAL, updateScope)
+	timer = Timer(INTERVAL, updateScopes)
 	timer.start()
 
 if __name__ == "__main__":
-	updateScope()
+	updateScopes()
 	app.run()
--- fetch_scope.py
+++ fetch_scope.py
@@ -3,8 +3,7 @@ from os import path
 import requests
 import tweepy
 import datetime
-
-QUERY = 'periscope sunset'
+import re
 
 # Returns (consumer_key, consumer_secret)
 def load_twitter_secrets():
@@ -20,24 +19,37 @@ twitter_auth = load_auth()
 twitter = tweepy.API(twitter_auth)
 
 # Returns tweet object
-def most_recent_live_scope():
-    tweets = twitter.search(q=QUERY)
+def most_recent_scope(query):
+    all_scopes = []
+    tweets = twitter.search(q='periscope ' + query)
     for tweet in tweets:
         print tweet.text
+        print "\n"
         for url in tweet.entities['urls']:
             expanded_url = url['expanded_url']
+            print expanded_url
             if expanded_url.startswith('https://www.periscope.tv/w/'):
-                if scope_is_live(expanded_url):
-                    return tweet, expanded_url
-    for tweet in tweets:
-        for url in tweet.entities['urls']:
-            expanded_url = url['expanded_url']
-            if expanded_url.startswith('https://www.periscope.tv/w/'):
-                return tweet, expanded_url
-    return "no scope found :("
+                response = requests.get(expanded_url)
+                scope_info = (expanded_url, start_time(response), scope_is_live(response))
+                if scope_info not in all_scopes:
+                    all_scopes.append(scope_info)
+    sorted_scopes = sorted(all_scopes, key=lambda x: x[1], reverse=True)
+    print sorted_scopes
+    live_scopes = [x for x in sorted_scopes if x[2] is True]
+    if len(live_scopes) > 0:
+        return live_scopes[0][0]
+    elif len(sorted_scopes) > 0:
+        return sorted_scopes[0][0]
+    else:
+        return None
 
-def scope_is_live(expanded_url):
-    return 'isEnded&quot;:false' in requests.get(expanded_url)
+
+def scope_is_live(response):
+    return 'isEnded&quot;:false' in response.text
+
+def start_time(response):
+    m = re.search(r'start&quot;:&quot;((\d\d\d\d)-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d))', response.text)
+    return m.group(1)
 
 def scope_in_timeframe(tweet, hours):
     return datetime.datetime.now() - tweet.created_at < datetime.timedelta(hours = hours)
