ee112b366ea4c42035ac6f8e892f87a05887a96c
helenanderson <helenanderson@stanford.edu>
2017-01-29 01:28:00 -0800

    refresh button done

--- app.py
+++ app.py
@@ -1,55 +1,85 @@
 from flask import Flask, Markup
 from threading import Timer
-from fetch_scope import most_recent_scope, scope_is_live
+from fetch_scope import most_recent_scopes, scope_is_live
 from flask import render_template, jsonify
 import requests
 import os
 
 app = Flask(__name__)
 
-stylingscript = "<script type='text/javascript'> var hideTwitterAttempts = 0; function hideTwitterBoxElements() { setTimeout( function() { $('#tweetembeddiv /deep/ .Tweet-header, #tweetembeddiv /deep/ .Tweet-text, #tweetembeddiv /deep/ .Tweet-metadata, #tweetembeddiv /deep/ .Tweet-actions, #tweetembeddiv /deep/ .SummaryCard-content').attr('style', 'display:none !important;'); $('#tweetembeddiv /deep/ .EmbeddedTweet').attr('style', 'border:0px !important;'); $('#tweetembeddiv /deep/ .EmbeddedTweet-tweet').attr('style', 'padding: 20px;'); $('#tweetembeddiv /deep/ .Tweet-body, #tweetembeddiv /deep/ .Tweet-card').attr('style', 'margin:0px !important;');$('#tweetembeddiv /deep/ .TwitterCardsGrid-col--spacerBottom').attr('style', 'margin:0px !important;'); hideTwitterAttempts++; if ( hideTwitterAttempts < 3 ) { hideTwitterBoxElements(); } }, 1500); } hideTwitterBoxElements(); </script>"
+stylingscript = "<script type='text/javascript'> $('#tweetembeddiv').hide(); var hideTwitterAttempts = 0; function hideTwitterBoxElements() { setTimeout( function() { $('#tweetembeddiv /deep/ .Tweet-header, #tweetembeddiv /deep/ .Tweet-text, #tweetembeddiv /deep/ .Tweet-metadata, #tweetembeddiv /deep/ .Tweet-actions, #tweetembeddiv /deep/ .SummaryCard-content, #tweetembeddiv /deep/ .EmbeddedTweet-ancestor').attr('style', 'display:none !important;'); $('#tweetembeddiv /deep/ .EmbeddedTweet').attr('style', 'border:0px !important;'); $('#tweetembeddiv /deep/ .EmbeddedTweet-tweet').attr('style', 'padding: 20px;'); $('#tweetembeddiv /deep/ .Tweet-body, #tweetembeddiv /deep/ .Tweet-card').attr('style', 'margin:0px !important;');$('#tweetembeddiv /deep/ .TwitterCardsGrid-col--spacerBottom').attr('style', 'margin:0px !important;'); if( !$('#tweetembeddiv /deep/ .Tweet-card').length) {$('#refresh').trigger('click');} else {$('#tweetembeddiv').show();}}, 1500); } hideTwitterBoxElements(); </script>"
 
 # There's probably a better way to do this where we don't have to keep track of both of these things globally, but for now...
 current_scopes = {
-	'sunrise': None,
-	'sunset': None
+	'sunrise': [],
+	'sunset': []
 }
-INTERVAL = 60.0
+current_sun_direction = "sunrise"
+INTERVAL = 30.0
 
-def constructEmbedHTML(key):
+error_message = "<div style='width:30%;margin-top:10%;text-align:center;'><h1 style='color:#DB4F4F;'>SORRY, WE COULDN'T FIND A " + current_sun_direction.upper() + " FOR YOU!</h1><h1> TRY REFRESHING, OR CHECK BACK LATER.</h1></div>"
+
+def constructEmbedHTML(key, scope):
 	global current_scopes
 	global stylingscript
-	current_tweet = current_scopes[key][0]
+	global error_message
+	if not scope:
+		return error_message
+	current_tweet = scope[0]
 	embed_url = 'https://api.twitter.com/1.1/statuses/oembed.json?id=' + str(current_tweet.id)
 	oembed = requests.get(embed_url).json()
+	# mark that we've seen this Scope
+	current_scopes[key][current_scopes[key].index(scope)][4] = True
 	return oembed['html'] + stylingscript
 
-@app.route('/_sets')
-def sets():
-	tweetHtml = constructEmbedHTML('sunset')
-	print tweetHtml
+def findFirstUnseen(sunDirection):
+	global current_scopes
+	unseen = [scope for scope in current_scopes[sunDirection] if scope[4] is False]
+	if len(unseen) is 0:
+		for scope in current_scopes[sunDirection]:
+			scope[4] = False
+		return current_scopes[sunDirection][0]
+	return unseen[0]
+
+@app.route('/_refreshsunrise')
+def refreshsunrise():
+	first_unseen = findFirstUnseen('sunrise')
+	tweetHtml = constructEmbedHTML('sunrise', first_unseen)
 	return jsonify(result=tweetHtml)
 
-@app.route('/_rises')
-def rises():
-	tweetHtml = constructEmbedHTML('sunrise')
+@app.route('/_refreshsunset')
+def refreshsunset():
+	first_unseen = findFirstUnseen('sunset')
+	tweetHtml = constructEmbedHTML('sunset', first_unseen)
+	return jsonify(result=tweetHtml)
+
+@app.route('/_sunset')
+def sunset():
+	global current_sun_direction
+	tweetHtml = constructEmbedHTML('sunset', current_scopes['sunset'][0])
 	print tweetHtml
+	current_sun_direction = "sunset"
+	return jsonify(result=tweetHtml)
+
+@app.route('/_sunrise')
+def sunrise():
+	global current_sun_direction
+	tweetHtml = constructEmbedHTML('sunrise', current_scopes['sunrise'][0])
+	current_sun_direction = "sunrise"
 	return jsonify(result=tweetHtml)
 
 @app.route("/")
 def home():
 	global current_scopes
-	initial_tweet_html = Markup(constructEmbedHTML('sunrise'))
+	global current_sun_direction
+	initial_tweet_html = Markup(constructEmbedHTML('sunrise', current_scopes['sunrise'][0]))
 	return render_template('home.html', scope=initial_tweet_html)
 
 def updateScopes():
 	global current_scopes
 
 	for key in current_scopes:
-		# If the current scope is still live, don't bother changing it
-		current_scope = current_scopes[key]
-		if current_scope is None or not scope_is_live(requests.get(current_scope[1])):
-			current_scopes[key] = most_recent_scope(key)
+		current_scopes[key] = most_recent_scopes(key, current_scopes[key])
 
 	timer = Timer(INTERVAL, updateScopes)
 	timer.start()
--- fetch_scope.py
+++ fetch_scope.py
@@ -6,50 +6,62 @@ import datetime
 import re
 
 #Returns (consumer_key, consumer_secret)
-def load_twitter_secrets():
-    twitter_filename = path.join('secrets', 'twitter.txt')
-    with open(twitter_filename, 'r') as twitter_file:
-        return twitter_file.read().strip().split('\n')
+# def load_twitter_secrets():
+#     twitter_filename = path.join('secrets', 'twitter.txt')
+#     with open(twitter_filename, 'r') as twitter_file:
+#         return twitter_file.read().strip().split('\n')
 
 def load_auth():
-    # consumer_key, consumer_secret = load_twitter_secrets()
-    # return tweepy.AppAuthHandler(consumer_key, consumer_secret)
+    #consumer_key, consumer_secret = load_twitter_secrets()
+    #return tweepy.AppAuthHandler(consumer_key, consumer_secret)
     return tweepy.AppAuthHandler(os.environ['TWITTER_KEY'], os.environ['TWITTER_SECRET'])
 
 twitter_auth = load_auth()
 twitter = tweepy.API(twitter_auth)
 
 # Returns scope_info object
-def most_recent_scope(query):
-    all_scopes = []
+def most_recent_scopes(query, scopes):
+    all_scopes = scopes
     tweets = twitter.search(q='periscope ' + query)
     for tweet in tweets:
-        #print tweet.text
-        # print "\n"
-        for url in tweet.entities['urls']:
-            expanded_url = url['expanded_url']
-            #print expanded_url
-            if expanded_url.startswith('https://www.periscope.tv/w/'):
-                response = requests.get(expanded_url)
-                scope_info = (tweet, expanded_url, start_time(response), scope_is_live(response))
-                if scope_info not in all_scopes:
-                    all_scopes.append(scope_info)
-    sorted_scopes = sorted(all_scopes, key=lambda x: x[2], reverse=True)
-    print [(scope[2], scope[3]) for scope in sorted_scopes]
-    live_scopes = [x for x in sorted_scopes if x[3] is True]
-    if len(live_scopes) > 0:
-        return live_scopes[0]
-    elif len(sorted_scopes) > 0:
-        return sorted_scopes[0]
-    else:
-        return None
+        original_text = ""
+        if hasattr(tweet, 'retweeted_status'):
+            original_text = tweet.retweeted_status.text
+        else:
+            original_text = tweet.text
 
+        for url in tweet.entities['urls']:
+            original_text = original_text.replace(url['url'], '')
+
+        if original_text not in [thing[5] for thing in all_scopes]:
+            #print tweet.text
+            for url in tweet.entities['urls']:
+                #print tweet.entities['urls']
+                plain_url = url['url']
+                expanded_url = url['expanded_url']
+                original_text.replace(plain_url, "")
+                #print expanded_url
+                if expanded_url.startswith('https://www.periscope.tv/w/'):
+                    response = requests.get(expanded_url)
+                    scope_info = [tweet, plain_url, start_time(response), scope_is_live(response), False, original_text]
+                    if scope_info[2] and (len(all_scopes) is 0 or scope_info[1] not in [scope[1] for scope in all_scopes]):
+                        all_scopes.append(scope_info)
+    sorted_scopes = sorted(all_scopes, key=lambda x: x[2], reverse=True)
+    for scope in sorted_scopes:
+        if scope[3] is True:
+            sorted_scopes.remove(scope)
+            sorted_scopes.insert(0, scope)
+    #print [(scope[1], scope[2], scope[3]) for scope in sorted_scopes]
+    #live_scopes = [x for x in sorted_scopes if x[3] is True]
+    return sorted_scopes
 
 def scope_is_live(response):
     return 'isEnded&quot;:false' in response.text
 
 def start_time(response):
     m = re.search(r'start&quot;:&quot;((\d\d\d\d)-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d))', response.text)
+    if not m:
+        return None
     return m.group(1)
 
 def scope_in_timeframe(tweet, hours):
